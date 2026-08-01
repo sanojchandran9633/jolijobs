5506e93b8f1c204f80c9b2ffe290c11d5c0411fb
sanoj <sanojchandran02@gmail.com>
2026-08-01 14:06:11 +0530

    First commit with all files

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {}
+variable "public_subnet_cidr" {}
+variable "private_subnet_cidr" {}
+variable "pub_name" {
+  default = "Pub_Subnet"
+}
+variable "pri_name" {
+  default = "Pri_Subnet"
+}
